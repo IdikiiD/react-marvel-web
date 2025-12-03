88f43ca7b75133f06bbe962f19daf863bab0cde5
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-03 11:49:30 -0800

    refactor RandomChar component to use functional component and hooks

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -2,53 +2,75 @@ import './randomChar.scss';
 
 import mjolnir from '../../resources/img/mjolnir.png';
 import MarvelService from "../../server/Server";
-import {Component} from "react";
+import {Component, useEffect, useRef, useState} from "react";
 import Spinner from "../../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
-class RandomChar extends Component {
-    state = {
-        char: {},
-        loading: true,
-        error: false
-    }
+const RandomChar = () => {
+    const [char, setChar] = useState(null);
+    const [loading , setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
-    componentDidMount() {
-        this.updateChar();
-    }
+    useEffect(() => {
+        updateChar();
+    },[])
 
-    marvelService = new MarvelService();
 
-    updateChar = () => {
-        this.setState({ loading: true,error: false },() => {
+
+    const marvelService = useRef(new MarvelService());
+
+    const updateChar = () => {
+
+            setLoading(true);
+            setError(false);
             const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-            this.marvelService
+
+            marvelService.current
                 .getCharacter(id)
-                .then(this.onCharLoaded)
-                .catch(this.errorMes);
-        });
+                .then(onCharLoaded)
+                .catch(errorMes);
+
 
     }
-    onCharLoaded = (char) => {
-        this.setState({
-            char,
-            loading: false,
-            error: false
-        })
+    const onCharLoaded = (char) => {
+        setChar(char)
+        setLoading(false);
+        setError(false);
     }
 
-    errorMes = () =>{
-        this.setState({
-            loading: false,
-            error:true})
+    const errorMes = () =>{
+        setLoading(false);
+        setError(true);
     }
 
 
-    render() {
-        const {char, loading, error} = this.state;
+
+
         const spinner = loading ? <Spinner/> : null
         const errorMessage = error ? <ErrorMessage/> : null
         const content = !(loading || error) ? <View char={char}/> : null
+
+    const View = ({char}) =>{
+        const {name, description, thumbnail, homepage, wiki} = char
+        return (
+            <div className="randomchar__block">
+                <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                <div className="randomchar__info">
+                    <p className="randomchar__name">{name}</p>
+                    <p className="randomchar__descr">
+                        {description}
+                    </p>
+                    <div className="randomchar__btns">
+                        <a href={homepage} className="button button__main">
+                            <div className="inner">homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">wiki</div>
+                        </a>
+                    </div>
+                </div>
+            </div>)
+    }
 
         return (
             <div className="randomchar">
@@ -63,7 +85,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main" onClick={this.updateChar}>
+                    <button className="button button__main" onClick={updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -72,29 +94,9 @@ class RandomChar extends Component {
         )
 
 
-    }
+
 }
-    const View = ({char}) =>{
-    const {name, description, thumbnail, homepage, wiki} = char
-    return (
-        <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-            <div className="randomchar__info">
-                <p className="randomchar__name">{name}</p>
-                <p className="randomchar__descr">
-                    {description}
-                </p>
-                <div className="randomchar__btns">
-                    <a href={homepage} className="button button__main">
-                        <div className="inner">homepage</div>
-                    </a>
-                    <a href={wiki} className="button button__secondary">
-                        <div className="inner">wiki</div>
-                    </a>
-                </div>
-            </div>
-        </div>)
-    }
+
 
 
 
