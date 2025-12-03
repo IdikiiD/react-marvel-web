f1da07fe8ed328711aef19f8d7abaaca1723f4e9
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-03 11:49:10 -0800

    refactor App component to use functional component and hooks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -8,19 +8,16 @@ import decoration from '../../resources/img/vision.png';
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
-class App extends Component {
+const App = ()=> {
 
-    state = {
-        selectedChar: null
+    const [selectedChar, setSelectedChar] = useState(null);
+
+
+    const onCharSelected = (id) => {
+        setSelectedChar(id)
     }
 
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
-        })
-    }
 
-    render() {
         return (
             <div className="app">
                 <AppHeader/>
@@ -30,17 +27,17 @@ class App extends Component {
                     </ErrorBoundary>
                     <div className="char__content">
                             <ErrorBoundary>
-                        <CharList onCharSelected={this.onCharSelected}/>
+                        <CharList onCharSelected={onCharSelected}/>
                             </ErrorBoundary>
                                 <ErrorBoundary>
-                        <CharInfo charId={this.state.selectedChar}/>
+                        <CharInfo charId={selectedChar}/>
                                     </ErrorBoundary>
                                 </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
             </div>
         )
-    }
+
 }
 
 export default App;
