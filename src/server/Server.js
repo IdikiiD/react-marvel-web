88849d8a68dbe40bca86604bfe1b218132aeaa3d
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-03 11:49:35 -0800

    refactor MarvelService to use functional component and hooks

--- src/server/Server.js
+++ src/server/Server.js
@@ -1,10 +1,10 @@
 
 
-class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/'
-    _apiKey = 'apikey=a59d4f3c86d00be5489803bebb31aef9'
-    _baseOffset = 210
-    getResource = async (url) =>{
+const MarvelService = () =>  {
+    const _apiBase = 'https://gateway.marvel.com:443/v1/public/'
+    const _apiKey = 'apikey=a59d4f3c86d00be5489803bebb31aef9'
+    const _baseOffset = 210
+    const getResource = async (url) =>{
         let res = await fetch(url)
 
         if(!res.ok){
@@ -13,18 +13,18 @@ class MarvelService {
         return await res.json()
     }
 
-    getAllCharacters = async (offset = this._baseOffset) => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+    const getAllCharacters = async (offset = _baseOffset) => {
+        const res = await getResource(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
         console.log('API Response:', res)
-        return res.data.results.map(this._transformCaracter);
+        return res.data.results.map(_transformCaracter);
     }
 
-    getCharacter = async (id) => {
-        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
-        return this._transformCaracter(res.data.results[0])
+    const getCharacter = async (id) => {
+        const res = await getResource(`${_apiBase}characters/${id}?${_apiKey}`)
+        return _transformCaracter(res.data.results[0])
     }
 
-    _transformCaracter = (char)=>{
+    const _transformCaracter = (char)=>{
         return{
             id: char.id,
             name:char.name,
@@ -36,6 +36,7 @@ class MarvelService {
         }
 
     }
+    return { getAllCharacters, getCharacter }
 
 }
 
