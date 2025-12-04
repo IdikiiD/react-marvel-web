6877329509c61cf6f773a78b127e5b89fdd2534c
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-04 15:51:15 -0800

    add useHttp hook for simplified HTTP requests and error handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./server/Server";
 import "./style/style.scss";
-
-const marvelService = new MarvelService();
-
-marvelService.getCharacter('1010338').then(char => console.log(char.name))
 
 const container = document.getElementById("root");
 const root = createRoot(container);
