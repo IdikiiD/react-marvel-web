282e64d51107d2c7eac61e19ea4f69dadb727dd8
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-04 15:51:29 -0800

    refactor MarvelService to use useHttp for API requests and simplify character data transformation

--- src/server/Server.js
+++ src/server/Server.js
@@ -1,43 +1,39 @@
+import { useHttp } from "../hooks/http.hook";
 
+const useMarvelService = () => {
+    const { loading, request, error, clearError } = useHttp();
 
-const MarvelService = () =>  {
-    const _apiBase = 'https://gateway.marvel.com:443/v1/public/'
-    const _apiKey = 'apikey=a59d4f3c86d00be5489803bebb31aef9'
-    const _baseOffset = 210
-    const getResource = async (url) =>{
-        let res = await fetch(url)
+    const _jsonUrl = '/heroes.json'; // путь к JSON в public
 
-        if(!res.ok){
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
-        }
-        return await res.json()
-    }
-
-    const getAllCharacters = async (offset = _baseOffset) => {
-        const res = await getResource(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
-        console.log('API Response:', res)
-        return res.data.results.map(_transformCaracter);
-    }
+    const getAllCharacters = async (offset = 0, limit = 9) => {
+        const data = await request(_jsonUrl);
+        // имитация offset и limit
+        const slice = data.slice(offset, offset + limit);
+        return slice.map(_transformCharacter);
+    };
 
     const getCharacter = async (id) => {
-        const res = await getResource(`${_apiBase}characters/${id}?${_apiKey}`)
-        return _transformCaracter(res.data.results[0])
-    }
+        const data = await request(_jsonUrl);
+        const char = data.find(c => c.id === id);
+        if (!char) throw new Error("Character not found");
+        return _transformCharacter(char);
+    };
 
-    const _transformCaracter = (char)=>{
-        return{
+    const _transformCharacter = (char) => {
+        return {
             id: char.id,
-            name:char.name,
-            description:char.description ? ` ${char.description.slice(0,210)}...`: 'There is no description about hero',
-            thumbnail:char.thumbnail.path + '.' + char.thumbnail.extension,
-            homepage:char.urls[0].url,
-            wiki:char.urls[1].url,
-            comics: char.comics.items
-        }
+            name: char.name,
+            description: char.description
+                ? `${char.description.slice(0, 210)}...`
+                : "There is no description for this character",
+            thumbnail: char.thumbnail,
+            homepage: char.homepage,
+            wiki: char.wiki,
+            comics: char.comics,
+        };
+    };
 
-    }
-    return { getAllCharacters, getCharacter }
+    return { loading, error, clearError, getAllCharacters, getCharacter };
+};
 
-}
-
-export default MarvelService
+export default useMarvelService;
