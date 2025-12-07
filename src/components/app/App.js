a96e0ab3204ad65d48e9b650a58e6b68366b8803
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-07 15:26:06 -0800

    feat(App): implement code-splitting for CharList, ComicsList, and SingleComic components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,23 @@
-import {useState} from "react";
+import {lazy, Suspense, useState} from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
+
 import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
 
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import ComicsList from "../comicsList/ComicsList";
-import SingleComic from "../singleComic/SingleComic";
+import Spinner from "../../spinner/Spinner";
+
+
+
+const CharList = lazy(()=> import('../charList/CharList'));
+const ComicsList = lazy(()=> import('../comicsList/ComicsList'));
+const SingleComic = lazy(()=> import('../singleComic/SingleComic'));
+
+const CharListPreloading = lazy(()=> import('../charList/CharList'));
+
 
 const App = () => {
 
@@ -26,6 +34,7 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
+                    <Suspense fallback={<Spinner/>}>
                     <Routes>
                         <Route path="/" element={
                             <>
@@ -46,6 +55,7 @@ const App = () => {
                         <Route path="/comics" element={<ComicsList onCharSelected={onCharSelected}/>}/>
                         <Route path="/comics/:id" element={<SingleComic />} />
                     </Routes>
+                    </Suspense>
                 </main>
             </div>
         </BrowserRouter>
