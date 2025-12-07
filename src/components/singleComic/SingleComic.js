ddc8b357abe4f9cdb7b1eb03617d60d620dd10fa
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-06 17:38:19 -0800

    feat(SingleComic): implement comic data fetching and rendering logic

--- src/components/singleComic/SingleComic.js
+++ src/components/singleComic/SingleComic.js
@@ -1,19 +1,58 @@
 import './singleComic.scss';
 import xMen from '../../resources/img/x-men.png';
+import {useEffect, useState} from "react";
+import useMarvelService from "../../server/Server";
+import {NavLink, useParams} from "react-router-dom";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import Spinner from "../../spinner/Spinner";
 
 const SingleComic = () => {
-    return (
-        <div className="single-comic">
-            <img src={xMen} alt="x-men" className="single-comic__img"/>
-            <div className="single-comic__info">
-                <h2 className="single-comic__name">X-Men: Days of Future Past</h2>
-                <p className="single-comic__descr">Re-live the legendary first journey into the dystopian future of 2013 - where Sentinels stalk the Earth, and the X-Men are humanity's only hope...until they die! Also featuring the first appearance of Alpha Flight, the return of the Wendigo, the history of the X-Men from Cyclops himself...and a demon for Christmas!?</p>
-                <p className="single-comic__descr">144 pages</p>
-                <p className="single-comic__descr">Language: en-us</p>
-                <div className="single-comic__price">9.99$</div>
+
+    const [comictData, setComictData] = useState(null);
+    const [newItemLoading, setNewItemLoading] = useState(false);
+
+    const {loading, error, getComic} = useMarvelService();
+    const {id} = useParams();
+
+    useEffect(() => {
+        onRequest(id)
+    }, [id])
+
+    const onRequest = (id) => {
+        getComic(id)
+            .then(onComictLoaded)
+
+    }
+    const onComictLoaded = (comictData) => {
+        setComictData(comictData);
+    }
+
+    const renderItems = (comictData) => {
+        return (
+            <div className="single-comic">
+                <img src={xMen} alt="x-men" className="single-comic__img"/>
+                <div className="single-comic__info">
+                    <h2 className="single-comic__name">{comictData.name}</h2>
+                    <p className="single-comic__descr">{comictData.description}</p>
+                    <p className="single-comic__descr">{comictData.pageCount}</p>
+                    <p className="single-comic__descr">{comictData.language}</p>
+                    <div className="single-comic__price">{comictData.price}</div>
+                </div>
+                <NavLink end to={"/comics"}  className="single-comic__back">Back to all</NavLink>
             </div>
-            <a href="#" className="single-comic__back">Back to all</a>
-        </div>
+        )
+    }
+    const errorMessage = error ? <ErrorMessage/> : null
+    const spinner = loading && !newItemLoading ? <Spinner/> : null
+
+    const content = !(loading || error || !comictData) ? renderItems(comictData) : null;
+
+    return (
+        <>
+            {errorMessage}
+            {spinner}
+            {content}
+        </>
     )
 }
 
