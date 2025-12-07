3f46a2bed9a3fb5f8fdc0994339fed0d4fe999e3
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-06 17:37:36 -0800

    feat(App): implement routing for comics and character details

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -7,8 +7,11 @@ import CharInfo from "../charInfo/CharInfo";
 import decoration from '../../resources/img/vision.png';
 
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import ComicsList from "../comicsList/ComicsList";
+import SingleComic from "../singleComic/SingleComic";
 
-const App = ()=> {
+const App = () => {
 
     const [selectedChar, setSelectedChar] = useState(null);
 
@@ -18,25 +21,35 @@ const App = ()=> {
     }
 
 
-        return (
+    return (
+        <BrowserRouter>
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <ErrorBoundary>
-                        <RandomChar/>
-                    </ErrorBoundary>
-                    <div className="char__content">
-                            <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                            </ErrorBoundary>
+                    <Routes>
+                        <Route path="/" element={
+                            <>
                                 <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
+                                    <RandomChar/>
+                                </ErrorBoundary>
+                                <div className="char__content">
+                                    <ErrorBoundary>
+                                        <CharList onCharSelected={onCharSelected}/>
                                     </ErrorBoundary>
+                                    <ErrorBoundary>
+                                        <CharInfo charId={selectedChar}/>
+                                    </ErrorBoundary>
+                                    <img className="bg-decoration" src={decoration} alt="vision"/>
                                 </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                            </>
+                        }/>
+                        <Route path="/comics" element={<ComicsList onCharSelected={onCharSelected}/>}/>
+                        <Route path="/comics/:id" element={<SingleComic />} />
+                    </Routes>
                 </main>
             </div>
-        )
+        </BrowserRouter>
+    )
 
 }
 
