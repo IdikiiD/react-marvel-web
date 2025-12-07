a3846d95d7206e40afb85d7bdf50d8f1a7aa50c6
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-06 17:37:41 -0800

    feat(AppHeader): replace anchor tags with NavLink for routing

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,4 +1,5 @@
 import './appHeader.scss';
+import {NavLink} from "react-router-dom";
 
 const AppHeader = () => {
     return (
@@ -10,9 +11,9 @@ const AppHeader = () => {
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="#">Characters</a></li>
+                    <li><NavLink end  to="/" style={({ isActive }) => ({ color: isActive ? '#9f0013' : '#000' })}>Characters</NavLink></li>
                     /
-                    <li><a href="#">Comics</a></li>
+                    <li><NavLink end  to="/comics" style={({ isActive }) => ({ color: isActive ? '#9f0013' : '#000' })}>Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
