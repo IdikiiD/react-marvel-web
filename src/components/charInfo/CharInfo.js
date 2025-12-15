600862ed0eb1c7da2ae33a087d49969b67451e16
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-15 22:15:48 +0200

    feat(CharInfo): refactor character info component for improved readability and prop validation

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,52 +1,43 @@
-import './charInfo.scss';
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-import {useState, useEffect, useCallback, memo} from "react";
 
-import ErrorMessage from "../errorMessage/ErrorMessage";
-import Spinner from "../../spinner/Spinner";
+
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton';
 
-import useComicVineService from "../../server/Server";
+import './charInfo.scss';
 import useMarvelService from "../../server/Server";
+import Spinner from "../../spinner/Spinner";
 
+const CharInfo = (props) => {
 
-
-const  CharInfo = memo(({charId}) => {
     const [char, setChar] = useState(null);
-
-
 
     const {loading, error, getCharacter, clearError} = useMarvelService();
 
-
     useEffect(() => {
         updateChar()
-    }, [updateChar])
+    }, [props.charId])
 
-
-    const updateChar = useCallback(() => {
-
+    const updateChar = () => {
+        const {charId} = props;
         if (!charId) {
-            return
+            return;
         }
-        clearError()
+
+        clearError();
         getCharacter(charId)
-            .then((char=>setChar(char)))
+            .then(onCharLoaded)
+    }
 
+    const onCharLoaded = (char) => {
+        setChar(char);
+    }
 
-    },[charId,clearError,getCharacter])
-
-
-
-
-
-
-
-
-
-    const skeleton = char || loading || error ? null : <Skeleton/>
-    const errorMessage = error ? <ErrorMessage/> : null
-    const spinner = loading ? <Spinner/> : null
+    const skeleton = char || loading || error ? null : <Skeleton/>;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
     const content = !(loading || error || !char) ? <View char={char}/> : null;
 
     return (
@@ -57,21 +48,20 @@ const  CharInfo = memo(({charId}) => {
             {content}
         </div>
     )
+}
 
-
-})
-
-
-const View =({char})=> {
+const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char;
+
     let imgStyle = {'objectFit' : 'cover'};
     if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
         imgStyle = {'objectFit' : 'contain'};
     }
-    return(
+
+    return (
         <>
             <div className="char__basics">
-                <img src={thumbnail} alt={name} style = {imgStyle}/>
+                <img src={thumbnail} alt={name} style={imgStyle}/>
                 <div>
                     <div className="char__info-name">{name}</div>
                     <div className="char__btns">
@@ -86,25 +76,28 @@ const View =({char})=> {
             </div>
             <div className="char__descr">
                 {description}
-
             </div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
-                {comics.length === 0 ? "There are no comics with this character" : null}
+                {comics.length > 0 ? null : 'There is no comics with this character'}
                 {
                     comics.map((item, i) => {
-                            if(i > 9)return
-                            return(
-                                <li key={i} className="char__comics-item">
-                                    {item.name}
-                                </li>
-                            )
-                        }
-                    )
+                        // eslint-disable-next-line
+                        if (i > 9) return;
+                        return (
+                            <li key={i} className="char__comics-item">
+                                {item.name}
+                            </li>
+                        )
+                    })
                 }
             </ul>
         </>
     )
 }
 
-export default CharInfo;
+CharInfo.propTypes = {
+    charId: PropTypes.number
+}
+
+export default CharInfo
